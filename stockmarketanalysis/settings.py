c93bb10aeba40ad7515ae15d232d3c1a9c31a8e5
Samarthvg <samarthgalchar@gmail.com>
2021-10-03 00:34:19 +0530

    security reasons

--- stockmarketanalysis/settings.py
+++ stockmarketanalysis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ml-c689mo&7u*t#0hcv43ef12ti5bi@h(#5m!hzn$@6!#9yk6w'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
